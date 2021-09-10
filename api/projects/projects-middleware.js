7187e52473d42ac0ae9fcb3323f65c2a84812d13
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-09-10 15:23:37 -0400

    Two test failing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,32 @@
 // add middlewares here related to projects
+const Projects = require("./projects-model")
+
+const checkProjectId = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const project = await Projects.get(id)
+        if (!project) {
+            res.status(404).json(`project ${id} not found`)
+        } else {
+            req.project = project
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({ message: `Error: ${err}` })
+    }
+}
+
+const checkProjectBody = async (req, res, next) => {
+    try {
+        const project = req.body
+        if (!project.name || !project.description) {
+            res.status(400).json({ message: "Name and description required" })
+        } else {
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({ message: `Error: ${err}` })
+    }
+}
+
+module.exports = { checkProjectId, checkProjectBody }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,69 @@
 // Write your "projects" router here!
+const express = require("express")
+
+const router = express.Router()
+
+const Projects = require("./projects-model")
+const { checkProjectId, checkProjectBody } = require("./projects-middleware")
+
+router.get("/", (req, res) => {
+    Projects.get()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(err => {
+            res.status(500).json({ message: err })
+        })
+})
+
+router.get("/:id", checkProjectId, async (req, res) => {
+    try {
+        const project = await Projects.get(req.params.id)
+        res.status(200).json(project)
+    } catch (err) {
+        res.status(500).json({ message: err })
+    }
+})
+
+router.post("/", checkProjectBody, async (req, res) => {
+    try {
+        const newProject = await Projects.insert(req.body)
+        res.status(201).json(newProject)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+})
+
+router.put("/:id", checkProjectId, checkProjectBody, async (req, res) => {
+    try {
+        const { id } = req.params
+        const changes = req.body
+        const editedProject = await Projects.update(id, changes)
+        res.status(200).json(editedProject)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.delete("/:id", checkProjectId, async (req, res) => {
+    try {
+        const { id } = req.params
+        const deletedProject = await Projects.remove(id)
+        res.status(200).json(deletedProject)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get("/:id/actions", checkProjectId, async (req, res) => {
+    try {
+        const project = await Projects.get(req.params.id)
+        console.log(project.actions)
+        res.status(200).json(project.actions)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
